d0de6b209d038f1f0b291199d007daa6e6793a60
wangxs940102 <wxs_it@163.com>
2020-01-10 15:36:26 +0800

    增加上传文件微服务

---
+++ ly-upload/src/main/java/com/leyou/upload/web/UplodaImagnController.java
@@ -0,0 +1,9 @@
+package com.leyou.upload.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class UplodaImagnController {
+}
